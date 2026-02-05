35651569c1901e33328d6f27c136c3c228b49374
EliodaCoding <eliottgiroud@gmail.com>
2026-02-04 17:00:36 -0800

    Trying ModEvents following tutorial. SUCCESS

--- src/main/java/net/eli/tutorialmod/event/ModEvents.java
+++ src/main/java/net/eli/tutorialmod/event/ModEvents.java
@@ -3,9 +3,15 @@ package net.eli.tutorialmod.event;
 import net.eli.tutorialmod.TutorialMod;
 import net.eli.tutorialmod.item.custom.HammerItem;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -39,6 +45,17 @@ public class ModEvents {
                 HARVESTED_BLOCKS.add(pos);
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLivingDamage(LivingDamageEvent event) {
+        if(event.getEntity() instanceof Sheep sheep && event.getSource().getDirectEntity() instanceof Player player) {
+            if(player.getMainHandItem().getItem() == Items.STICK) {
+                player.sendSystemMessage(Component.literal(player.getName().getString() + " JUST HIT A SHEEP WITH STICK! we took the stick away"));
+                sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 5));
+                player.getMainHandItem().shrink(1);
             }
         }
     }
